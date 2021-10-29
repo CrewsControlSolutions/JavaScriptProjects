eb3f9e8ed7cbe8f1d47a0cc8fe03d4ae4ff7438e
CrewsControlSolutions <92337423+CrewsControlSolutions@users.noreply.github.com>
2021-10-29 11:48:43 -0600

    Modified HTML and JS files for dictionary

---
+++ BasicsPart1/Dictionaries/JS/main.js
@@ -0,0 +1,14 @@
+//this is my first created dictionary consisting of Key-Value-Pairs (KVPs)
+function myDictionary() {
+    var Virus = {
+        Type: "SARS-COV-2",
+        Carrier: 'human',
+        Origin: "Wuhan, China",
+        Creation: "gain of function",
+        Age: 1,
+        Purpose: "biological weapon",
+        Age: 2, //the key called "Age" is now paired with a value of 2 and the original value is no longer represented
+    };
+    delete Virus.Origin; //The mainstream media does exactly this--they delete key facts
+    document.getElementById('dictionary').innerHTML=Virus.Origin;
+}
